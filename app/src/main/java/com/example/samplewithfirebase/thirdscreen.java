e91d5fe73ea0bba0d552bab8ce20f5ced383f894
Robert Bao <robertsbao@gmail.com>
2022-05-20 15:10:08 -0400

    Add third screen

---
+++ app/src/main/java/com/example/samplewithfirebase/thirdscreen.java
@@ -0,0 +1,14 @@
+package com.example.samplewithfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class thirdscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thirdscreen);
+    }
+}
