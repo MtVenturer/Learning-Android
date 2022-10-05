bcc22e51786641226ec284e55eae531d7b75f70f
Robert Bao <robertsbao@gmail.com>
2022-10-05 10:12:16 -0500

    remove quote fetching

--- app/src/main/java/com/example/samplewithfirebase/MainActivity.java
+++ app/src/main/java/com/example/samplewithfirebase/MainActivity.java
@@ -29,41 +29,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mQuoteTextView = (TextView) findViewById(R.id.textView2);
+//        mQuoteTextView = (TextView) findViewById(R.id.textView2);
     }
 
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-
-        Map<String, Object> dataToSave=new HashMap<String,Object>();
-        dataToSave.put(QUOTE_KEY,message);
-        mDocRef.set(dataToSave).addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void unused) {
-                Log.d("quotes","Document saved");
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Log.w("quotes","document not saved");
-            }
-        });
-    }
-    public void fetchQuote(View view){
-        mDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-            @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                if (documentSnapshot.exists()) {
-                    String quoteText = documentSnapshot.getString(QUOTE_KEY);
-                    mQuoteTextView.setText(quoteText);
-                }
-            }
-        });
-    }
+//    public void sendMessage(View view) {
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+//
+//        Map<String, Object> dataToSave=new HashMap<String,Object>();
+//        dataToSave.put(QUOTE_KEY,message);
+//        mDocRef.set(dataToSave).addOnSuccessListener(new OnSuccessListener<Void>() {
+//            @Override
+//            public void onSuccess(Void unused) {
+//                Log.d("quotes","Document saved");
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                Log.w("quotes","document not saved");
+//            }
+//        });
+//    }
+//    public void fetchQuote(View view){
+//        mDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+//            @Override
+//            public void onSuccess(DocumentSnapshot documentSnapshot) {
+//                if (documentSnapshot.exists()) {
+//                    String quoteText = documentSnapshot.getString(QUOTE_KEY);
+//                    mQuoteTextView.setText(quoteText);
+//                }
+//            }
+//        });
+//    }
 
     public void uploadScreen(View view){
         Intent intent = new Intent(this, photoScreen.class);
